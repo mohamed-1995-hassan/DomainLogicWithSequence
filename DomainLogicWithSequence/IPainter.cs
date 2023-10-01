02c87ebc30795e858b71e999dfd72489edceddb8
Mohamed Hassan <sunsherp@gmail.com>
2023-10-01 12:20:15 +0300

    Add project files.

---
+++ DomainLogicWithSequence/IPainter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLogicWithSequence
+{
+    public interface IPainter
+    {
+        bool IsAvailable { get; }
+        TimeSpan EstimateTimeToPoint(double sqMeters);
+        double EstimateCompensation(double sqMeters);
+    }
+}
---
+++ DomainLogicWithSequence/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using DomainLogicWithSequence;
+
+static IPainter FindCheapestPaint(double sqMeters, IEnumerable<IPainter> painters)
+{
+	double bestPrice = 0;
+	IPainter cheapest = null;
+	foreach (IPainter painter in painters)
+	{
+		if (painter.IsAvailable)
+		{
+			double price = painter.EstimateCompensation(sqMeters);
+			if (cheapest == null || price < bestPrice)
+			{
+				cheapest = painter;
+            }
+        }
+	}
+	return cheapest;
+}
+static void main(string[] args)
+{
+
+}
